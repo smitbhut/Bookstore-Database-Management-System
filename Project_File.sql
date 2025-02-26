8c8ec05645954c86ddd765d422791fca2017bb3c
smitbhut <157902740+smitbhut@users.noreply.github.com>
2025-02-26 16:25:22 +0530

    Update Project_File.sql

--- Project_File.sql
+++ Project_File.sql
@@ -41,17 +41,17 @@ SELECT * FROM Orders;
 
 -- Import Data into Books Table
 COPY Books(Book_ID, Title, Author, Genre, Published_Year, Price, Stock) 
-FROM 'D:\Course Updates\30 Day Series\SQL\CSV\Books.csv' 
+FROM 'D:\sem-5\SQL Project\SQL_Resume_Project\Books.csv' 
 CSV HEADER;
 
 -- Import Data into Customers Table
 COPY Customers(Customer_ID, Name, Email, Phone, City, Country) 
-FROM 'D:\Course Updates\30 Day Series\SQL\CSV\Customers.csv' 
+FROM 'D:\sem-5\SQL Project\SQL_Resume_Project\Customers.csv' 
 CSV HEADER;
 
 -- Import Data into Orders Table
 COPY Orders(Order_ID, Customer_ID, Book_ID, Order_Date, Quantity, Total_Amount) 
-FROM 'D:\Course Updates\30 Day Series\SQL\CSV\Orders.csv' 
+FROM 'D:\sem-5\SQL Project\SQL_Resume_Project\Orders.csv' 
 CSV HEADER;
 
 
